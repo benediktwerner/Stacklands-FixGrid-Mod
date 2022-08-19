0591a26f40ccedd5e01ea1fdbd702eaf9cbae42c
Benedikt Werner <1benediktwerner@gmail.com>
2022-08-19 22:43:41 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace FixGrid
+{
+    [BepInPlugin(
+        "de.benediktwerner.stacklands.fixgrid",
+        PluginInfo.PLUGIN_NAME,
+        PluginInfo.PLUGIN_VERSION
+    )]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<float> MinGridWidth;
+        public static ConfigEntry<float> MinGridHeight;
+
+        private void Awake()
+        {
+            MinGridWidth = Config.Bind(
+                "General",
+                "Minimum Grid Cell Width",
+                0.7f,
+                "Vanilla is 0.75"
+            );
+            MinGridHeight = Config.Bind(
+                "General",
+                "Minimum Grid Cell Height",
+                0.85f,
+                "Vanilla is 0.85"
+            );
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(WorldManager), nameof(WorldManager.Update))]
+        [HarmonyPrefix]
+        public static void FixGridSize(WorldManager __instance)
+        {
+            if (__instance.CurrentBoard == null)
+                return;
+
+            var width = __instance.CurrentBoard.WorldBounds.extents.x - 0.5f;
+            var xCount = (int)(width / MinGridWidth.Value);
+            __instance.GridWidth = width / xCount;
+
+            var height = __instance.CurrentBoard.WorldBounds.extents.z - 0.5f;
+            var zCount = (int)(height / MinGridHeight.Value);
+            __instance.GridHeight = height / zCount;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Stacklands FixGrid Mod
+
+Adjusts the card alignment grid (when pressing E to align card) so that it always perfectly aligns with the map
+meaning you'll never have cards trying to align into the wall and jump around because they can't again.
+
+Specifically, it increases the space between cards in the grid if necessary to make it align.
+
+It also adds configuration options for the minimum size of the grid and by default slightly decreases the minimal horizontal distance between
+cards. You can change the configuration via the Mod Manager or by editing the `BepInEx/config/de.benediktwerner.stacklands.FixGrid.cfg` file
+which will be generated after starting the game with the mod installed for the first time.
+
+## Manual Installation
+This mod requires BepInEx to work. BepInEx is a modding framework which allows multiple mods to be loaded.
+
+1. Download and install BepInEx from the [Thunderstore](https://stacklands.thunderstore.io/package/BepInEx/BepInExPack_Stacklands/).
+4. Download this mod and extract it into `BepInEx/plugins/`
+5. Launch the game
+
+## Links
+- Github: https://github.com/benediktwerner/Stacklands-FixGrid-Mod
+- Thunderstore: https://stacklands.thunderstore.io/package/benediktwerner/FixGrid
+
+## Changelog
+
+- v1.0: Initial release
